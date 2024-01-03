a65ec7a47a301e53a9cff235b519ab0ccc6e54e7
Ankit Gupta <iambittuankitbkg@gmail.com>
2024-01-03 22:23:14 +0530

    backend final touch

--- app.js
+++ app.js
@@ -4,6 +4,7 @@ import userRouter from "./router/user.js";
 import taskRouter from "./router/task.js";
 import cookieParser from "cookie-parser";
 import { errorMiddleware } from "./middleware/error.js";
+import cors from "cors";
 
 config({ path: "./config.env" });
 
@@ -12,6 +13,14 @@ export const app = express();
 //middelware
 app.use(express.json());
 app.use(cookieParser());
+app.use(
+  cors({
+    origin: [process.env.FRONTEND_URI],
+    methods: ["GET", "POST", "PUT", "DELETE"],
+    credentials: true,
+  })
+);
+
 app.use("/api/v1/user", userRouter);
 app.use("/api/v1/task", taskRouter);
 
